e2dab4f49d9199c1c1c6aae577cf03c5de4e3a35
baggedmilk999 <89410031+baggedmilk999@users.noreply.github.com>
2022-01-27 11:28:25 -0500

    Add files via upload

---
+++ Scope JS/scope.js
@@ -0,0 +1,57 @@
+//slide 2
+const city = 'New York City';
+
+const logCitySkyline = () => {
+  let skyscraper = 'Empire State Building';
+  return 'The stars over the ' + skyscraper + ' in ' + city;
+};
+
+console.log(logCitySkyline());
+
+//slide 3
+const city = 'New York City';
+
+const logCitySkyline = () => {
+  let skyscraper = 'Empire State Building';
+  return 'The stars over the ' + skyscraper + ' in ' + city;
+};
+
+console.log(logCitySkyline());
+
+//slide 4
+const logVisibleLightWaves = () => {
+    const lightWaves = 'Moonlight';
+    console.log(lightWaves);
+  };
+  
+  logVisibleLightWaves();
+  
+  // console.log(lightWaves);
+
+  //slide 5
+  const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+let stars = 'North Star';
+
+const callMyNightSky = () => {
+  stars = 'Sirius';
+	return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
+};
+
+console.log(callMyNightSky());
+console.log(stars);
+
+//slide 6
+const logVisibleLightWaves = () => {
+    let lightWaves = 'Moonlight';
+      let region = 'The Arctic';
+    // Add if statement here:
+    if (region === 'The Arctic'){
+      let lightWaves = 'Northern Lights';
+      console.log(lightWaves);
+    }
+    
+    console.log(lightWaves);
+  };
+  
+  logVisibleLightWaves();
